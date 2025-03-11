dc3f43bd7e20128083edffccbffa8692efff6123
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-03-11 14:46:52 +0600

    template

---
+++ template.js
@@ -0,0 +1,27 @@
+// const first = "jaan";
+// const last = "pakhi";
+
+// const nameOf = first + " " + last; 
+
+// console.log(nameOf);
+
+// const a = 10 ;
+// const b = 20 ; 
+// const result = "The sum of " + a + " and " + b + " is: " + (a+b); //normal 
+// const math = `The sum of ${a} and ${b} is: ${a+b}` //ES6 using  backtick 
+// console.log(result);
+// console.log(math);
+
+function abc(){
+    console.log(abc.xyz);
+    
+}
+
+abc()
+abc.xyz = 400;
+abc.xyz = 200;
+abc()
+
+
+
+
---
+++ var_let_const.js
@@ -0,0 +1,23 @@
+// const money = 50;
+
+// // money = 70;
+
+// const rich = money + 10;
+
+// console.log(rich)
+
+// const numbers = [12,343,33,55,5];
+// numbers[3] = 44;
+// numbers.push(23,443)
+// console.log(numbers);
+
+
+let sum = 0; 
+for ( let i=0;i<10; i++){
+   const num= i;
+   sum = sum + num; 
+
+}
+console.log(sum);
+
+
